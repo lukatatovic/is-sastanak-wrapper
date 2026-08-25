524f0b2fcc9801b88c4eb68462e042fa583d2ec8
lukatatovic <tatovicluka@gmail.com>
2026-08-25 20:46:58 +0200

    add meetings service

---
+++ meetings-service/src/main/java/rs/vs/meetings_service/MeetingsServiceApplication.java
@@ -0,0 +1,13 @@
+package rs.vs.meetings_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingsServiceApplication.class, args);
+	}
+
+}
---
+++ meetings-service/src/test/java/rs/vs/meetings_service/MeetingsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.vs.meetings_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
