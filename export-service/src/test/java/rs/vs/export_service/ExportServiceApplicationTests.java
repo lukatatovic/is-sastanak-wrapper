fae90d20e9de08b46dbe2fce827f7d65046f0c91
lukatatovic <tatovicluka@gmail.com>
2026-08-27 13:21:01 +0200

    add export service project

---
+++ export-service/src/test/java/rs/vs/export_service/ExportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.vs.export_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
